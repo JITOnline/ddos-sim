40de45873bb628fe7e050fe128758b45a3a835ae
flee122 <67753369+flee122@users.noreply.github.com>
2020-07-31 08:37:53 -0400

    Update ddos-sim.sh

--- ddos-sim.sh
+++ ddos-sim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gunicorn -w 2 -b 0.0.0.0:8080 --error-logfile gunicorn.error.log --access-logfile gunicorn.log ddosSim:app
+gunicorn --certfile cert.pem --keyfile key.pem -w 2 -b 0.0.0.0:8080 --error-logfile gunicorn.error.log --access-logfile gunicorn.log ddosSim:app
 #gunicorn -w 2 -b 0.0.0.0:8080 ddosSim:app
 
 #export FLASK_APP=ddosSim.py
